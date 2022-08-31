5b8d67e46ba3a28cd8081158adc8b17f369e4c63
Aftertaste <cosperda@gmail.com>
2022-08-31 07:59:31 -0400

    Update LOServer.sh

--- LOServer.sh
+++ LOServer.sh
@@ -1,23 +1,86 @@
 #!/bin/bash
 # chmod +x this script, then run it
 # Needs work, i know, its basic
+# tested Ubuntu 20.04.4-live 64 bit
+# tested Debian 11.3.0-amd64 bit
 ########################################################################
-SERVER_INSTALL_DIR=/home/Aftertaste/LOServer # Change this to your install dir
+SERVER_INSTALL_DIR=/home/dcosper/LOServer # Change this to your install dir
 APP_ID=920720 # Update this if the ID changes, or if you find season 4 id to use instead
 ###################DO NOT CHANGE ANYTHING BELOW THIS LINE!!!############
+
+debian64()
+{
+	sudo sed -i 's/main/main contrib non-free/' /etc/apt/sources.list
+	sudo dpkg --add-architecture i386
+	sudo apt update
+	sudo apt install steamcmd
+}
+ubuntu64()
+{
+	sudo add-apt-repository multiverse && sudo apt install software-properties-common -y && sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install lib32gcc-s1 steamcmd -y
+}
+steamcmdinstall()
+{
+	steamcmd +force_install_dir $SERVER_INSTALL_DIR +login anonymous +app_update $APP_ID validate +exit
+	echo "$APP_ID" > $SERVER_INSTALL_DIR/Mist/Binaries/Linux/steam_appid.txt
+}
+
 SCVERSION=$(apt-cache policy steamcmd | grep Installed)
-BIT=x86_86
-if [[ "$SCVERSION" = *"(none)"* ]];
+
+DISTRO="Unsupported"
+
+SYSTEM="Debian"
+
+BIT="32"
+
+if [[ "$SCVERSION" = *"(none)"* || "$SCVERSION" = "" ]];
 	then
-		BIT=$(uname -m)
-		if [[ "$BIT" = *"64"* ]];
-		then
-			echo "Setting up for 64 bit SteamCmd"
-				sudo add-apt-repository multiverse && sudo apt install software-properties-common -y && sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install lib32gcc-s1 steamcmd -y
-		else
-			echo "Setting up for 32 bit SteamCmd"
-			sudo apt install steamcmd -y
-		fi
+		DISTRO=$(uname -a)
+	else
+		steamcmdinstall
+		exit 0
 fi
-steamcmd +force_install_dir $SERVER_INSTALL_DIR +login anonymous +app_update $APP_ID validate +exit
-echo "$APP_ID" > $SERVER_INSTALL_DIR/Mist/Binaries/Linux/steam_appid.txt
+
+if [[ "$DISTRO" = "Unsupported" ]];
+	then
+		echo "Unsupported system, install manually."
+			exit 0
+	else
+		if [[ "$DISTRO" = *"Debian"* && "$DISTRO" = *"64"* ]];
+			then
+				SYSTEM="Debian"
+				BIT="64"
+				fi
+		if [[ "$DISTRO" = *"Ubuntu"* && "$DISTRO" = *"64"* ]];
+			then
+				SYSTEM="Ubuntu"
+				BIT="64"
+				fi
+fi
+
+# Works do distro specific stuff here
+
+if [[ "$SYSTEM" = "Debian" ]];
+	then
+	if [[ "$BIT" = "64" ]];
+		then
+		debian64
+	else if [[ "$BIT" = "32" ]];
+		then
+		echo "32 bit Debian"
+		fi
+	fi
+fi
+if [[ "$SYSTEM" = "Ubuntu" ]];
+	then
+	if [[ "$BIT" = "64" ]];
+		then
+		ubuntu64
+	else if [[ "$BIT" = "32" ]];
+		then
+		echo "32 bit Ubutu"
+		fi
+	fi
+fi
+
+steamcmdinstall
